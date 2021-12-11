aa5b840a04c323cb3d65512c65e91736839f3b6b
Mario Uher <uher.mario@gmail.com>
2021-12-11 13:56:07 +0100

    Implement part 2 of day 9

---
+++ 09/part2.exs
@@ -0,0 +1,56 @@
+Code.require_file("../advent_of_code_2021.ex")
+
+defmodule Day09.Part2 do
+  def is_low_point?(map, {x, y}) do
+    point = Map.get(map, {x, y})
+    north = Map.get(map, {x, y - 1})
+    east = Map.get(map, {x + 1, y})
+    south = Map.get(map, {x, y + 1})
+    west = Map.get(map, {x - 1, y})
+
+    (is_nil(north) || north > point) and
+      (is_nil(east) || east > point) and
+      (is_nil(south) || south > point) and
+      (is_nil(west) || west > point)
+  end
+
+  def find_low_point(map, {x, y}) do
+    point = Map.get(map, {x, y})
+    north = Map.get(map, {x, y - 1})
+    east = Map.get(map, {x + 1, y})
+    south = Map.get(map, {x, y + 1})
+    west = Map.get(map, {x - 1, y})
+
+    cond do
+      Day09.Part2.is_low_point?(map, {x, y}) -> {x, y}
+      !is_nil(north) && point > north -> Day09.Part2.find_low_point(map, {x, y - 1})
+      !is_nil(east) && point > east -> Day09.Part2.find_low_point(map, {x + 1, y})
+      !is_nil(south) && point > south -> Day09.Part2.find_low_point(map, {x, y + 1})
+      !is_nil(west) && point > west -> Day09.Part2.find_low_point(map, {x - 1, y})
+    end
+  end
+end
+
+map =
+  AdventOfCode2021.stream_input_file()
+  |> Enum.map(fn line -> line |> String.codepoints() |> Enum.map(&String.to_integer/1) end)
+  |> Enum.with_index()
+  |> Enum.flat_map(fn {row, y} ->
+    row |> Enum.with_index() |> Enum.map(fn {point, x} -> {{x, y}, point} end)
+  end)
+  |> Map.new()
+
+map
+|> Enum.reduce(%{}, fn {{x, y}, point}, acc ->
+  if point == 9 do
+    acc
+  else
+    low_point = Day09.Part2.find_low_point(map, {x, y})
+    Map.update(acc, low_point, 1, &(&1 + 1))
+  end
+end)
+|> Map.values()
+|> Enum.sort(&>=/2)
+|> Enum.take(3)
+|> Enum.product()
+|> IO.inspect(label: "result")
