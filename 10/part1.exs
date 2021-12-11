f8d4df70254fbf012439ed5274af23d751bb96a7
Mario Uher <uher.mario@gmail.com>
2021-12-11 14:13:44 +0100

    Implement part 1 of day 10

---
+++ 10/part1.exs
@@ -0,0 +1,31 @@
+Code.require_file("../advent_of_code_2021.ex")
+
+defmodule Day10.Part1 do
+  def parse(line, stack \\ [])
+
+  def parse([], _), do: :ok
+
+  def parse([char | rest], stack) do
+    case {char, stack} do
+      {"[", _} -> parse(rest, ["]" | stack])
+      {"(", _} -> parse(rest, [")" | stack])
+      {"{", _} -> parse(rest, ["}" | stack])
+      {"<", _} -> parse(rest, [">" | stack])
+      {"]", ["]" | stack]} -> parse(rest, stack)
+      {")", [")" | stack]} -> parse(rest, stack)
+      {"}", ["}" | stack]} -> parse(rest, stack)
+      {">", [">" | stack]} -> parse(rest, stack)
+      {invalid, [expected | _]} -> {:error, {expected, invalid}}
+    end
+  end
+end
+
+points = %{")" => 3, "]" => 57, "}" => 1197, ">" => 25137}
+
+AdventOfCode2021.stream_input_file()
+|> Enum.map(&String.codepoints/1)
+|> Enum.map(&Day10.Part1.parse(&1))
+|> Enum.reject(&(&1 == :ok))
+|> Enum.map(fn {:error, {_, invalid}} -> points[invalid] end)
+|> Enum.sum()
+|> IO.inspect(label: "result")
